7b41d61ce3e5dd680ff4d5bb5c28c1031a1d768c
Uni0305 <git@uni0305.me>
2022-11-03 13:20:07 +0900

    Feat: Impl landing page for large devices

--- components/LinkedIcon.tsx
+++ components/LinkedIcon.tsx
@@ -10,10 +10,10 @@ export default function LinkedIcon({ icon, href }: Props) {
   return (
     <Link href={href} target="_blank">
       <Icon
-        className="invert dark:invert-0"
+        className="h-4 sm:h-6 invert dark:invert-0"
         icon={icon}
         color="#fff"
-        height={16}
+        height="100%"
       />
     </Link>
   );
