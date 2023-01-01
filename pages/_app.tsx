84e8e221c82077af875c1d6a26ea3aedee10249b
Uni0305 <git@uni0305.me>
2023-01-01 21:35:57 +0900

    Feat: Install Google Tag for Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,32 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
+
+const GoogleAnalytics = () => {
+  const { GA_MEASUREMENT_ID } = process.env;
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        
+        gtag('config', ${GA_MEASUREMENT_ID});
+      `}</Script>
+    </>
+  );
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GoogleAnalytics />
+      <Component {...pageProps} />;
+    </>
+  );
 }
